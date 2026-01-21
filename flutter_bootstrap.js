4b5f086823e4ce2eb30dcc8f65953a6085aad3b7
VanceHud <VanceHud@users.noreply.github.com>
2026-01-21 04:08:39 +0000

    deploy: bacb36e88554bae6124e25eb2fcb526cb76c056d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4108658802"
+    serviceWorkerVersion: "2262132305"
   }
 });
